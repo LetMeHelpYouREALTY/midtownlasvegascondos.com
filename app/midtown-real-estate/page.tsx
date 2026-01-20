770fdeb22ce3c957af0c5ebe7bb1b84b70fb88aa
genekellyboyle <DrDuffy@bhhsnv.com>
2026-01-20 07:39:53 -0800

    Add explicit price range props to RealScoutSearch widget on Midtown Real Estate page

--- app/midtown-real-estate/page.tsx
+++ app/midtown-real-estate/page.tsx
@@ -183,7 +183,7 @@ export default function MidtownRealEstatePage() {
             </p>
           </div>
           <div className="max-w-4xl mx-auto">
-            <RealScoutSearch />
+            <RealScoutSearch priceMin="450000" priceMax="1000000" />
           </div>
         </section>
 
